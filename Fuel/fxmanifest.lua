4651c1cbbbfc741fcb25856b5687ad11c75d7170
Guudz <Kilian1702@outlook.com>
2023-02-03 05:31:51 +0100

    Initial commit

---
+++ Fuel/fxmanifest.lua
@@ -0,0 +1,23 @@
+-- New-LegacyFuel refait par Garfield, le plus bneau de tous les chats développeurs.
+
+fx_version 'adamant'
+game 'gta5'
+
+server_scripts {
+	'config.lua',
+	'source/fuel_server.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'source/fuel_client.lua'
+}
+
+exports {
+	'GetFuel',
+	'SetFuel'
+}
+
+
+
+client_script 'TGVnYWN5RnVlbA.lua'
